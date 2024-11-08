1b7f36d5ea730c4dfe7346f9b2944559a84a7e6e
Robert Hill <rhill@apple.com>
2024-11-07 18:03:36 -0800

    Updated defense macro

--- macros/helldivers-Bugs-Defense.py
+++ macros/helldivers-Bugs-Defense.py
@@ -4,28 +4,28 @@
 import lib.hell_constants as h
 
 app = {
-    'name' : 'Bug-Defense',
+    'name' : 'Bug-TowerDefense',
     'macros' : [
         # COLOR    LABEL        KEY SEQUENCE
         # 1st row ----------
-        (h.GREEN, 'AutoCan',   h.stratagem(h.AUTOCANNON)),
-        (h.GREEN, 'RailGun',   h.stratagem(h.RAILGUN)),
-        (h.GREEN, 'Stalwar',   h.stratagem(h.STALWART)),
+        (h.BLUE,    'AutoCan',  h.stratagem(h.AUTOCANNON)),
+        (h.BLUE,    'Recoil',   h.stratagem(h.RECOILLESS)),
+        (h.BLUE,    'Airbur',   h.stratagem(h.AIRBURST_ROCKET)),
 
         # 2nd row ----------
-        (h.GREEN, 'Expend',    h.stratagem(h.EXPENDABLE_ANTI_TANK)),
-        (h.GREEN, '4-shot',    h.stratagem(h.COMMANDO)),
-        (h.GREEN, 'Recoil',    h.stratagem(h.RECOILLESS)),
+        (h.BLUE,    'Expend',   h.stratagem(h.EXPENDABLE_ANTI_TANK)),
+        (h.BLUE,    '4-shot',   h.stratagem(h.COMMANDO)),
+        (h.GREEN,   'MG101Em',  h.stratagem(h.MG_101_EMPLACEMENT)),
 
         # 3rd row ----------
-        (h.GREEN, 'Airbur',    h.stratagem(h.AIRBURST_ROCKET)),
-        (h.GREEN, 'Spear',     h.stratagem(h.SPEAR)),
-        (h.GREEN, 'HeavyM',    h.stratagem(h.HEAVY_MACHINE_GUN)),
+        (h.RED,     '500',      h.stratagem(h.EAGLE_500KG)),
+        (h.RED,     'Staf',     h.stratagem(h.EAGLE_STRAFING_RUN)),
+        (h.RED,     'HeavyM',   h.stratagem(h.HEAVY_MACHINE_GUN)),
 
         # 4th row ----------
-        (h.GREEN, 'Grenad',    h.stratagem(h.GRENADE_LAUNCHER)),
-        (h.GREEN, 'Quasar',    h.stratagem(h.QUASAR_CANNON)),
-        (h.GREEN, 'Arc',       h.stratagem(h.ARC_THROWER)),
+        (h.GREEN,   'Mortr',    h.stratagem(h.MORTAR_SENTRY)),
+        (h.GREEN,   'GatSen',   h.stratagem(h.GATLING_SENTRY)),
+        (h.GREEN,   'TeslaT',   h.stratagem(h.TESLA_TOWER)),
         
         # Encoder button ---
         (0x000000, '',          []),
