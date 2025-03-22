bc4ee641ceaf022e044fa54d5e0765fe1b2994e1
pbuild <rhill@apple.com>
2025-03-21 19:21:19 -0700

    Add new hover pack

--- lib/hell_constants.py
+++ lib/hell_constants.py
@@ -36,6 +36,7 @@ LAS_5_GUARD_DOG         = {'color':BLUETWO, 'label':'Laz', 'keycode':(Keycode.S,
 GAS_GUARD_DOG           = {'color':BLUETWO, 'label':'Gas', 'keycode':(Keycode.S, Keycode.W, Keycode.A, Keycode.W, Keycode.D, Keycode.W)}
 DIRECTIONAL_SHIELD      = {'color':BLUETWO, 'label':'DShld', 'keycode':(Keycode.S, Keycode.W, Keycode.A, Keycode.D, Keycode.W, Keycode.W)}
 PORTABLE_HELLBOMB       = {'color':BLUETWO, 'label':'Helbm', 'keycode':(Keycode.S, Keycode.D, Keycode.W, Keycode.W, Keycode.W)}
+HOVER_PACK              = {'color':BLUETWO, 'label':'HverP', 'keycode':(Keycode.S, Keycode.W, Keycode.W, Keycode.S, Keycode.A, Keycode.D)}
 
 # SUPPORT WEAPONS -- BLUE
 MACHINE_GUN             = {'color':BLUE, 'label':'MaGun', 'keycode':(Keycode.S, Keycode.A, Keycode.S, Keycode.W, Keycode.D)}
